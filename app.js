f9865a801d0beff4d9222aad49dbba7c9c133f0e
Dilan Ozusta <dilanozusta@outlook.com>
2018-09-21 15:06:44 -0500

    sequelize essentials

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
 const express = require('express');
 const morgan = require('morgan');
-// const layout = require('layout')
 const layout = require('./views/layout')
+const models = require('./models');
+const userRouter = require('./routes/user');
+const wikiRouter = require('./routes/wiki');
 
+// {db} === models
 
+const PORT = 3000;
 
 const app = express();
-// app.layout();
-express.urlencoded({extended: false})
-express.static(__dirname+"/public")
+express.urlencoded({ extended: false })
+express.static(__dirname + "/public")
 
-app.get('/', function(req, res){
-    res.send(layout(''));
-    console.log('hi world terminal');
+app.get('/', function (req, res) {
+  res.send(layout(''));
+  // db.authenticate().
+  // then(() => {
+  //   console.log('connected to the database');
+  // })
+});
+
+app.use('/wiki', wikiRouter);
+
+const init = async () => {
+  await models.db.sync()
+  // more to come on server class
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}!`);
   });
-  
-  app.listen(3000);
+}
+
+models.db.sync({force: true});
+
+init();
